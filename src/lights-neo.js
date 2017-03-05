6a4c4180464d0e72b24fd115b3aa0d400fbeb38a
Ben Foxall <benfoxall@gmail.com>
2017-03-05 22:08:47 +0000

    Extracted NeoPixel class

--- src/lights-neo.js
+++ src/lights-neo.js
@@ -1,163 +1,142 @@
-var n = 64;
-var pix = new Uint8ClampedArray(n * 3);
+function NeoPixels(pin, length) {
+  this.pin = pin;
+  this.n = length;
+  this.data = new Uint8ClampedArray(length * 3);
+  this.sweeps = [];
+  this.interval = null;
+}
 
-function animate(r,g,b, callback) {
-  var start = getTime()
-  var duration = .5
-  var last = start
+NeoPixels.prototype.write = function() {
+  E.neopixelWrite(this.pin, this.data);
+};
 
-  var interval = setInterval(function () {
-    var now = getTime()
-    var progress = (now -  start) / duration
-    // progress = progress * progress
-    for (var i = 0; i < n; i++) {
-      if((i/n) < progress) {
-        pix[i*3    ] = r
-        pix[i*3 + 1] = g
-        pix[i*3 + 2] = b
-      }
+NeoPixels.prototype.set = function(i, c) {
+  if(i < this.n && i > 0) {
+    var r = (c >> 16) & 0xff;
+    var g = (c >> 8) & 0xff;
+    var b = c & 0xff;
+
+    this.data[i * 3]     = r;
+    this.data[i * 3 + 1] = g;
+    this.data[i * 3 + 2] = b;
+
+    this.write();
+  }
+};
+
+NeoPixels.prototype.sweep = function(c, delay) {
+  delay = (delay || 0) / 1000;
+
+  var r = (c >> 16) & 0xff;
+  var g = (c >> 8) & 0xff;
+  var b = c & 0xff;
+
+  this.sweeps.unshift([getTime() + delay, r, g, b]);
+
+  this.sweeps.sort((a,b) => a[0] < b[0] ? 1 : -1);
+
+  // kick off animation
+  if(!this.interval){
+    this.interval = setInterval(
+      this.animate.bind(this), 40
+    );
+  }
+};
+
+NeoPixels.prototype.animate = function(){
+  if(!this.sweeps.length) {
+    clearInterval(this.interval);
+    return (this.interval = null);
+  }
+
+  const now = getTime();
+  const duration = 3;
+
+  // sweep index
+  var si = 0;
+
+  // set the progress of each sweep
+  // (and update si if this has already passed) ugly
+  for (var i = 0; i < this.sweeps.length; i++)
+    if((this.sweeps[i][4] = (now - this.sweeps[i][0]) / duration) < 0)
+      si = i+1;
+
+  // nothing to animate yet
+  if(si > this.sweeps.length -1) return;
+
+  var progress;
+  for (i = 0; i < this.n; i++) {
+    progress = i/this.n;
+
+    // if we've gone past, step si
+    if(progress > this.sweeps[si][4]) {
+      si++;
+      if(si > this.sweeps.length -1) break;
     }
-    if(progress > 1) {
-      clearInterval(interval)
-      console.log("cleared")
-      if(callback) callback()
-    }
 
-    E.neopixelWrite(D1, pix)
-  }, 10)
+    this.data[i*3    ] = this.sweeps[si][1];
+    this.data[i*3 + 1] = this.sweeps[si][2];
+    this.data[i*3 + 2] = this.sweeps[si][3];
+
+  }
+
+  // remove finished sweeps
+  for (i = this.sweeps.length-1; i >= 0; i--) {
+    if(this.sweeps[i][4] > 1) {
+      this.sweeps.splice(i,1);
+    }
+  }
+
+  this.write();
+};
+
+
+var neopix = new NeoPixels(D1, 64);
+
+function animate(c){
+
+  if(c === undefined) c = 0xffffff;
+
+  var t = 0;
+
+  neopix.sweep(0xFF0000, t+=100);
+  neopix.sweep(0xFF7F00, t+=100);
+  neopix.sweep(0xFFFF00, t+=100);
+  neopix.sweep(0x96bf33, t+=100);
+  neopix.sweep(0x0000FF, t+=100);
+
+  neopix.sweep(c, t+=100);
+
+  neopix.sweep(0x000000, t+=2500);
+  neopix.sweep(0xFF0000, t+=100);
+  neopix.sweep(0xFFFF00, t+=100);
+  neopix.sweep(0x0000FF, t+=100);
+  neopix.sweep(0x000000, t+=100);
 
 }
 
-function go(){
-  animate(150,0,150, () => {
-    animate(255,255,0, () => {
-      animate(0,0,0)
-    })
-  })
-}
 
-go()
-
-
+const root_topic = '/lights/';
 
 // a list of topics to subscribe to
 const MQTT_subs = [
-  '/puck/btn'
-]
+  root_topic + '#'
+];
+
 
 // (implement this) - a handler for incoming messages
-function MQTT_handle(topic, message) {
-  go();
+function MQTT_handle(topic, payload) {
+  if(topic.indexOf(root_topic) === 0) {
+    var rest = topic.substr(root_topic.length);
+    var addr = parseInt(rest, 10);
+    var colr = parseInt((payload+'').replace('#',''), 16);
+
+    if(!isNaN(addr))  neopix.set(addr, colr);
+    if(rest == 'all') animate(colr);
+  }
 }
 
 
 
 
-
-//
-//
-// // generate data for a set of neopixels
-// function neoPixels(data, pixel) {
-//   for (var i = 0; i < n; i++) {
-//     var p = pixel(i);
-//     data[i * 3 + 0] = p[0];
-//     data[i * 3 + 1] = p[1];
-//     data[i * 3 + 2] = p[2];
-//   }
-// }
-//
-// function rainbow(data, start) {
-//   neoPixels(data, (i) => {
-//     var offset = start + ((i / 16) * Math.PI * 2);
-//     var r = (Math.sin(offset + 0) + 1) * 127;
-//     var g = (Math.sin(offset + 2) + 1) * 127;
-//     var b = (Math.sin(offset + 4) + 1) * 127;
-//     return [r,g,b];
-//   });
-// }
-//
-// function brightness(data, by) {
-//   for(var i = 0; i < data.length; i ++)
-//     data[i] *= by;
-// }
-//
-//
-// rainbow(pix, Date.now() / 5000); brightness(pix, 0.1);
-//
-// //E.setClock(80);
-// pinMode(D1, 'output');
-//
-// //neoPixels(pix, (i) => [10,0,0]);
-//
-// //E.neopixelWrite(D1, pix);
-//
-// var count = 0;
-// //
-// // while(count < 10) {
-// //   var t0 = getTime();
-// //   count++;
-// //   rainbow(pix, count / 20);
-// //   brightness(pix, 0.05);
-// //   var t1 = getTime();
-// //   E.neopixelWrite(D1, pix);
-// //
-// //   var t2 = getTime();
-// //
-// //   console.log(t1-t0, t2-t1);
-// // }
-// console.log("OK");
-//
-// neoPixels(pix, (i) => [10,0,0]);
-// E.neopixelWrite(D1, pix);
-//
-// setInterval(() => {
-//   E.neopixelWrite(D1, pix);
-// }, 500);
-//
-// /*
-// setInterval(function(){
-//   if(count++ % 2) {
-//     rainbow(pix, Date.now() / 5000); brightness(pix, 0.5);
-//   } else {
-//     E.neopixelWrite(D1, pix);
-//   }
-//
-//
-//   //count = (count + 1) % 16;
-//
-// //  neoPixels(pix, (i) => [0,i < count ? 0 : 30,0]);
-//
-//
-//
-// }, 10);
-// */
-
-
-/*setTimeout(function(){
-  setInterval(function(){
-    count = (count + 1) % 16;
-    neoPixels(pix, (i) => [0,i < count ? 0 : 30,0]);
-  }, 500);
-},250);
-*/
-
-
-/*
-
-var c = 0;
-setWatch(function() {
-
-  E.setClock(80);
-  pinMode(D1, 'output');
-
-  console.log("press - " + c++);
-  c = c % 16;
-
-  neoPixels(pix, (i) => [2,c > i ? 2 : 20,5]);
-
-  E.neopixelWrite(D1, pix);
-
-
-}, BTN, { repeat:true, edge:"rising", debounce: 50 });
-
-*/
+animate(0x0088ff);
